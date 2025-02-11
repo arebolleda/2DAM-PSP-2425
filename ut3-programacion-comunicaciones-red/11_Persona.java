f67c0f99e37c90a1096e9eed84241e89bfc293d8
Alfonso Rebolleda Sánchez <47109499+arebolleda@users.noreply.github.com>
2025-02-11 12:04:25 +0100

    Add files via upload

---
+++ ut3-programacion-comunicaciones-red/11_Persona.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+@SuppressWarnings("serial")
+
+public class Persona implements Serializable {
+	String nombre;
+	int edad;	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}	
+	public Persona() {super();}
+
+	public String getNombre() {return nombre;}
+	public void setNombre(String nombre) {this.nombre = nombre;}
+	public int getEdad() {return edad;}
+	public void setEdad(int edad) {this.edad = edad;}
+}
